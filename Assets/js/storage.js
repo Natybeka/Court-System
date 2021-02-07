b97f54e99bedec5efd18e7afbe358305b83ca901
Natnael Bekabtu <se.natnael.bekabtu@gmail.com>
2021-02-07 13:10:54 +0300

    Object Designs Tested

--- Assets/js/storage.js
+++ Assets/js/storage.js
@@ -1,19 +1,25 @@
 //Declaring UI Elements for data population
-
+const caseCollection = document.querySelector(".case-collection");
 
 //Objects Design
-function User(userName, password){
-    this.userName = userName;
-    this.password = password;
+
+// User Object has a name (Early design may be subject to change)
+function User(userName){
+    this.userName = userName; 
 }
 
-function Case(caseID, status, plaintiff, defendant, courtLevel) {
+/*  
+    Case Object design must have ID, status, plaintiff, defendant
+    and courtLevel (level at which the case is currently being heard)
+    finally a method for changing courtLevel and status
+*/
+function Case(caseID, status, plaintiff, defendant, judge, courtLevel) {
     this.caseID = caseID;
     this.status = status;
     this.plaintiff = plaintiff;
     this.defendant = defendant;
     this.courtLevel = courtLevel;
-    this.staffAttended = staffAttended;
+    this.judge = judge;
     
     this.changeStatus = function(newStatus) {
         if (newStatus != this.status) {
@@ -23,8 +29,31 @@ function Case(caseID, status, plaintiff, defendant, courtLevel) {
     
     this.changeCourtLevel = function(newCourtLevel) {
         if (this.courtLevel != newCourtLevel) {
-            
+            this.courtLevel = new courtLevel;
         }
     }
 
+    this.changeJudge = function(newJudge) {
+        this.judge = newJudge;
+    }
 }
+
+
+
+
+
+
+// let Court;
+// //Opening database upon document loading
+// document.addEventListener('DOMContentLoaded', () => {
+//     let db = indexedDB.open("CourtSystem", 1);
+
+//     // Initialize the database
+//     db.onupgradeneeded = function(e){
+//         db = e.target.result;
+//         if (!db.objectStoreNames.contains("Cases")) {
+//             db.createObjectStore("Cases");
+//         }
+
+//     } 
+// });
